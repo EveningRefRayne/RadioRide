2a6e4729e7a92f3d609d719f001bf190b75eb94d
ScottT4ylor <G00236985@smail.raritanval.edu>
2018-01-27 03:51:22 -0500

    Working on player movement
    Added floating movement, started adding ground movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float jumpPower = 1;
+    public float moveSpeed = 1;
+    public float accelTime = 0.1f;
+    public bool canJump = false;
+    public bool inWifiRange = false;
+    private Rigidbody phys;
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+    {
+        phys = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(inWifiRange)
+        {
+            phys.velocity = new Vector2(Mathf.Lerp(phys.velocity.x, Input.GetAxis("horizontal"), accelTime) * moveSpeed,
+                Mathf.Lerp(phys.velocity.y, Input.GetAxis("vertical"), accelTime) * moveSpeed);
+        }
+        else if(Input.GetAxis)
 	}
 }
